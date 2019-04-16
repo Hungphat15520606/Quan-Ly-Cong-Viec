da55b66b4252bb9474111f0b4b6122b5abd6fef3
zzphoghy <doivjpxx@gmail.com>
2019-04-16 16:01:46 +0700

    add some page

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import Container from "./layout/Container";
 
 export class Root extends Component {
   render() {
-    return(
-        <div>
-            <Header/>
-            {/* content is here */}
-            <Footer/>
-        </div>
+    return (
+      <div className="container">
+      <Header/>
+      <Container/>
+      <Footer/>
+      </div>
     );
   }
 }
+
+export default Root;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Home page.</h1>
+        <h2>built using React.js + React Router v4 + Express.js</h2>
+        <h2>Now, It is Server-side rendering !</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          iusto, voluptatem facilis eligendi suscipit aliquid, molestiae animi
+          maxime autem dolor exercitationem ad? Laboriosam quos maiores nihil. A
+          eligendi eaque provident. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ea minima a, velit necessitatibus recusandae,
+          praesentium quaerat autem neque est rerum vero vitae dolore deserunt
+          culpa cumque iste? Quos, voluptatem inventore? Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Nihil iste facilis velit eum neque
+          esse laborum rerum mollitia quas ratione, quia dolores earum magnam
+          exercitationem atque, sed accusantium nostrum quibusdam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Accusantium, rem
+          reprehenderit! Quod autem perferendis asperiores architecto corporis
+          non ipsam expedita odit adipisci assumenda. Et voluptates iusto
+          tempore, ex harum est.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          iusto, voluptatem facilis eligendi suscipit aliquid, molestiae animi
+          maxime autem dolor exercitationem ad? Laboriosam quos maiores nihil. A
+          eligendi eaque provident. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ea minima a, velit necessitatibus recusandae,
+          praesentium quaerat autem neque est rerum vero vitae dolore deserunt
+          culpa cumque iste? Quos, voluptatem inventore? Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Nihil iste facilis velit eum neque
+          esse laborum rerum mollitia quas ratione, quia dolores earum magnam
+          exercitationem atque, sed accusantium nostrum quibusdam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Accusantium, rem
+          reprehenderit! Quod autem perferendis asperiores architecto corporis
+          non ipsam expedita odit adipisci assumenda. Et voluptates iusto
+          tempore, ex harum est.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          iusto, voluptatem facilis eligendi suscipit aliquid, molestiae animi
+          maxime autem dolor exercitationem ad? Laboriosam quos maiores nihil. A
+          eligendi eaque provident. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ea minima a, velit necessitatibus recusandae,
+          praesentium quaerat autem neque est rerum vero vitae dolore deserunt
+          culpa cumque iste? Quos, voluptatem inventore? Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Nihil iste facilis velit eum neque
+          esse laborum rerum mollitia quas ratione, quia dolores earum magnam
+          exercitationem atque, sed accusantium nostrum quibusdam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Accusantium, rem
+          reprehenderit! Quod autem perferendis asperiores architecto corporis
+          non ipsam expedita odit adipisci assumenda. Et voluptates iusto
+          tempore, ex harum est.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          iusto, voluptatem facilis eligendi suscipit aliquid, molestiae animi
+          maxime autem dolor exercitationem ad? Laboriosam quos maiores nihil. A
+          eligendi eaque provident. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ea minima a, velit necessitatibus recusandae,
+          praesentium quaerat autem neque est rerum vero vitae dolore deserunt
+          culpa cumque iste? Quos, voluptatem inventore? Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Nihil iste facilis velit eum neque
+          esse laborum rerum mollitia quas ratione, quia dolores earum magnam
+          exercitationem atque, sed accusantium nostrum quibusdam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Accusantium, rem
+          reprehenderit! Quod autem perferendis asperiores architecto corporis
+          non ipsam expedita odit adipisci assumenda. Et voluptates iusto
+          tempore, ex harum est.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          iusto, voluptatem facilis eligendi suscipit aliquid, molestiae animi
+          maxime autem dolor exercitationem ad? Laboriosam quos maiores nihil. A
+          eligendi eaque provident. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ea minima a, velit necessitatibus recusandae,
+          praesentium quaerat autem neque est rerum vero vitae dolore deserunt
+          culpa cumque iste? Quos, voluptatem inventore? Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Nihil iste facilis velit eum neque
+          esse laborum rerum mollitia quas ratione, quia dolores earum magnam
+          exercitationem atque, sed accusantium nostrum quibusdam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Accusantium, rem
+          reprehenderit! Quod autem perferendis asperiores architecto corporis
+          non ipsam expedita odit adipisci assumenda. Et voluptates iusto
+          tempore, ex harum est.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
